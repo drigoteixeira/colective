9bf3855beb1fb91df21c66b5d2f404e201f9ac73
Rodrigo Teixeira <drigoteixeira@gmail.com>
2013-05-30 13:02:40 -0300

    Montagem do código HTML base.

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="pt-br"> <!--<![endif]-->
+<head>
+	
+	<!-- META -->
+	<meta charset="utf-8">
+	<title>Drigo Teixeira</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- VIEWPORT -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- FAVICON -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+</head>
+<body>
+
+	<div class="container">	
+		<header class="header sixteen columns">	
+			<h1><a href="/blog/" title="Drigo Teixeira">Drigo Teixeira</a></h1>
+			<p>UI Designer &amp; Entusiasta de Arquiteura de Informação</p>
+		</header>
+
+		<div class="content sixteen columns">
+			<section class="posts row">
+				<article class="post">
+					<h3 class="post-title"><a href="#">Título do post</a></h3>
+					<div class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fringilla leo enim, ut facilisis mi. Sed faucibus sagittis egestas. Duis diam diam, tristique a euismod nec, dictum sit amet lectus. Vivamus scelerisque porta urna quis volutpat. In hac habitasse platea dictumst. Praesent felis elit, sollicitudin et pulvinar vel, ornare non augue. Fusce in nibh ligula, in elementum nunc. Nulla facilisi. Duis fermentum, lacus vitae tincidunt blandit, diam enim vulputate tortor, non lobortis est augue non ante. Maecenas euismod ligula purus, sed venenatis nibh. Donec vitae ante at sem lacinia porta eget eu tellus. Aliquam vel dictum neque. Fusce justo elit, fringilla sit amet fringilla at, volutpat id nibh. Sed rutrum purus ipsum. Vestibulum malesuada purus vitae est rutrum pretium. 
+					</div>
+					<div class="post-meta">
+						<time class="post-date">07 Junho, 2013</time>
+					</div>
+				</article>
+			</section>
+			<section class="posts row">
+				<article class="post">
+					<h3 class="post-title"><a href="#">Título do post</a></h3>
+					<div class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fringilla leo enim, ut facilisis mi. Sed faucibus sagittis egestas. Duis diam diam, tristique a euismod nec, dictum sit amet lectus. Vivamus scelerisque porta urna quis volutpat. In hac habitasse platea dictumst. Praesent felis elit, sollicitudin et pulvinar vel, ornare non augue. Fusce in nibh ligula, in elementum nunc. Nulla facilisi. Duis fermentum, lacus vitae tincidunt blandit, diam enim vulputate tortor, non lobortis est augue non ante. Maecenas euismod ligula purus, sed venenatis nibh. Donec vitae ante at sem lacinia porta eget eu tellus. Aliquam vel dictum neque. Fusce justo elit, fringilla sit amet fringilla at, volutpat id nibh. Sed rutrum purus ipsum. Vestibulum malesuada purus vitae est rutrum pretium. 
+						<div class="post-meta">
+							<time class="post-date">07 Junho, 2013</time>
+						</div>
+					</div>
+				</article>
+			</section>
+			<section class="posts row">
+				<article class="post">
+					<h3 class="post-title"><a href="#">Título do post</a></h3>
+					<div class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fringilla leo enim, ut facilisis mi. Sed faucibus sagittis egestas. Duis diam diam, tristique a euismod nec, dictum sit amet lectus. Vivamus scelerisque porta urna quis volutpat. In hac habitasse platea dictumst. Praesent felis elit, sollicitudin et pulvinar vel, ornare non augue. Fusce in nibh ligula, in elementum nunc. Nulla facilisi. Duis fermentum, lacus vitae tincidunt blandit, diam enim vulputate tortor, non lobortis est augue non ante. Maecenas euismod ligula purus, sed venenatis nibh. Donec vitae ante at sem lacinia porta eget eu tellus. Aliquam vel dictum neque. Fusce justo elit, fringilla sit amet fringilla at, volutpat id nibh. Sed rutrum purus ipsum. Vestibulum malesuada purus vitae est rutrum pretium. 
+					</div>
+					<div class="post-meta">
+						<time class="post-date">07 Junho, 2013</time>
+					</div>
+				</article>
+			</section>
+			<section class="posts row">
+				<article class="post">
+					<h3 class="post-title"><a href="#">Título do post</a></h3>
+					<div class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fringilla leo enim, ut facilisis mi. Sed faucibus sagittis egestas. Duis diam diam, tristique a euismod nec, dictum sit amet lectus. Vivamus scelerisque porta urna quis volutpat. In hac habitasse platea dictumst. Praesent felis elit, sollicitudin et pulvinar vel, ornare non augue. Fusce in nibh ligula, in elementum nunc. Nulla facilisi. Duis fermentum, lacus vitae tincidunt blandit, diam enim vulputate tortor, non lobortis est augue non ante. Maecenas euismod ligula purus, sed venenatis nibh. Donec vitae ante at sem lacinia porta eget eu tellus. Aliquam vel dictum neque. Fusce justo elit, fringilla sit amet fringilla at, volutpat id nibh. Sed rutrum purus ipsum. Vestibulum malesuada purus vitae est rutrum pretium. 
+					</div>
+					<div class="post-meta">
+						<time class="post-date">07 Junho, 2013</time>
+					</div>
+				</article>
+			</section>
+			<section class="posts row">
+				<article class="post">
+					<h3 class="post-title"><a href="#">Título do post</a></h3>
+					<div class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fringilla leo enim, ut facilisis mi. Sed faucibus sagittis egestas. Duis diam diam, tristique a euismod nec, dictum sit amet lectus. Vivamus scelerisque porta urna quis volutpat. In hac habitasse platea dictumst. Praesent felis elit, sollicitudin et pulvinar vel, ornare non augue. Fusce in nibh ligula, in elementum nunc. Nulla facilisi. Duis fermentum, lacus vitae tincidunt blandit, diam enim vulputate tortor, non lobortis est augue non ante. Maecenas euismod ligula purus, sed venenatis nibh. Donec vitae ante at sem lacinia porta eget eu tellus. Aliquam vel dictum neque. Fusce justo elit, fringilla sit amet fringilla at, volutpat id nibh. Sed rutrum purus ipsum. Vestibulum malesuada purus vitae est rutrum pretium. 
+					</div>
+					<div class="post-meta">
+						<time class="post-date">07 Junho, 2013</time>
+					</div>
+				</article>
+			</section>
+		</div>
+			<footer class="content sixteen columns">
+			<a href="https://twitter.com/drigoteixeira" class="twitter-follow-button" data-show-count="false" data-lang="pt" data-size="large">Seguir @drigoteixeira</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	</footer>
+	</div>
+
+
+
+</body>
+</html>
