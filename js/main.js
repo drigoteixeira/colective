b145c975bdc0d921da7f6ab9ab0cd28543bc1efa
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2013-06-05 10:39:22 -0300

    Inclusão de funcionalidade para abertura de links em uma nova janela

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-// Twitter Button
+$(document).ready(function() {
+	
+	// ABRIR LINKS EM UMA NOVA JANELA
+	if(!$.browser.msie){
+		$("a[href*='http://']:not([href*='"+location.hostname+"']),[href*='https://']:not([href*='"+location.hostname+"'])").attr("target","_blank").addClass("external");
+	}
 
+});
