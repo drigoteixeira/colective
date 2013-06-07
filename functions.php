ba1c432cda48bba7c13116549322405663f53b5a
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2013-06-07 15:57:26 -0300

    Retirada dos comandos de remover componentes do wp_head

--- functions.php
+++ functions.php
@@ -11,22 +11,6 @@ function colective_setup() {
 	add_theme_support('post-thumbnails');
 	set_post_thumbnail_size( 600, 9999 ); 
 
-    // Remove itens da função wp_head();
-    remove_action('wp_head', 'feed_links', 2);
-    remove_action('wp_head', 'feed_links_extra', 3);
-    remove_action('wp_head', 'rsd_link');
-    remove_action('wp_head', 'wlwmanifest_link');
-    remove_action('wp_head', 'index_rel_link');
-    remove_action('wp_head', 'parent_post_rel_link');
-    remove_action('wp_head', 'start_post_rel_link');
-    remove_action('wp_head', 'adjacent_posts_rel_link');
-    // remove_action('wp_head', 'check_and_publish_future_post');
-    remove_action('wp_head', 'wp_print_styles');
-    remove_action('wp_head', 'wp_generator');
-    remove_action('wp_head', 'rel_canonical');
-    // remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
-
-
     /* Remove a Barra de Estilo de Admin */
     add_filter( 'show_admin_bar', '__return_false' );
     remove_action('wp_head', '_admin_bar_bump_cb');
