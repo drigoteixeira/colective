6a1f91e6b38e5cf5e2bcd717f3d45a9379c6fe99
Rodrigo Teixeira <drigoteixeira@gmail.com>
2013-06-07 23:32:39 -0300

    Retirada do meta description.

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
         <!-- METAS -->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
         
         <!-- WINDOWS 8 PINNED SITE -->
