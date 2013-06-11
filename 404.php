e3dc8f706a79a04f1d4342fc4081a67c421346af
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2013-06-11 09:30:13 -0300

    Ajuste no texto da página de erro 404

--- 404.php
+++ 404.php
@@ -5,11 +5,10 @@
 					<div class="c12">
 						<section class="posts">
 							<article class="post">
-								<h2 class="post-title">404 - Postagem não encontrada</h2>
+								<h2 class="post-title">404 - Página não encontrada</h2>
 								<div class="post-content">
-									<p>Unfortunately the content you're looking for isn't here. There may be a misspelling in your web address or you may have clicked a link for content that no longer exists. Perhaps you would be interested in our most recent articles.</p>
 
-									<p>Infelismente o que conteúdo que você estava procurando não está mais disponível. Isso pode ter acontecido por algum erro digitação na barra de endereços ou porque clicou em um link que não existe mais.</p>
+									<p>Infelizmente o que conteúdo que você estava procurando não está mais disponível ou foi removido. Por favor, acesse a <a href="/blog/">lista de postagens</a> para continuar a navegação. </p>
 
 								</div>
 							</article>
