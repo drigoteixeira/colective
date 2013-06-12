02d4de22367dccd1d7b30211ffad5e92be396866
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2013-06-12 15:21:53 -0300

    Ajuste na chamada do script complementar do botão do Twitter no Footer

--- footer.php
+++ footer.php
@@ -5,9 +5,7 @@
                         <div class="row">
                             <div class="c12">
                                 <div class="twitterBox">
-                                <a href="https://twitter.com/drigoteixeira" class="twitter-follow-button" data-show-count="false" data-lang="pt" data-size="large">Seguir @drigoteixeira</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-
+                                    <a href="https://twitter.com/drigoteixeira" class="twitter-follow-button" data-show-count="false" data-lang="pt" data-size="large">Seguir @drigoteixeira</a>
                                 </div>  
                             </div>
                         </div>
@@ -25,6 +23,7 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.10.0.min.js"><\/script>')</script>
         <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
         
     </body>
 </html>
