ddc4c894878d792c60b84b91db1253a05a84d7d0
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2013-06-14 11:03:59 -0300

    Ajuiste no posicionamento da data dos posts

--- category.php
+++ category.php
@@ -11,12 +11,12 @@
 
 						<section class="posts">
 							<article class="post">
+								<div class="post-meta">
+									<span class="post-date"><?php the_time('j M, Y') ?></span>
+								</div>
 								<h3 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 								<div class="post-content">
 									<?php the_excerpt(); ?>
-								</div>
-								<div class="post-meta">
-									<span class="post-date"><?php the_time('j M, Y') ?></span>
 								</div>
 							</article>
 						</section>
--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 			<div class="grid-12">
 				<section class="posts">
 					<article class="post">
+						<div class="post-meta">
+							<span class="post-date"><?php the_time('j M, Y') ?></span>
+						</div>
 						<h3 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 						<div class="post-content">
 							<?php the_excerpt(); ?>
-						</div>
-						<div class="post-meta">
-							<span class="post-date"><?php the_time('j M, Y') ?></span>
 						</div>
 					</article>
 				</section>
