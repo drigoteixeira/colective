7e6f4a331155911bd1059cab01f5a9c13450aceb
Rodrigo Teixeira <drigoteixeira@gmail.com>
2013-06-18 22:27:50 -0300

    Retirada do espaços de sobra do header.php

--- header.php
+++ header.php
@@ -4,36 +4,35 @@
 <!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-    <title><?php wp_title( '|', true, 'right' ); ?></title>
+<title><?php wp_title( '|', true, 'right' ); ?></title>
 
-    <!-- METAS -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width">
+<!-- METAS -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width">
 
-    <?php wp_head(); ?>
+<?php wp_head(); ?>
     
+<!-- GOOGLE+ -->
+<link rel="author" href="https://plus.google.com/u/0/110545419521358863582" />
 
-    <!-- GOOGLE+ -->
-    <link rel="author" href="https://plus.google.com/u/0/110545419521358863582" />
+<!-- FAVICONS -->
+<link rel="icon" href="favicon.ico">
 
-    <!-- FAVICONS -->
-    <link rel="icon" href="favicon.ico">
+<!-- CSS -->
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/2.1.0/normalize.css">
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/2.1.0/normalize.css">
+<!-- RSS -->
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 
-    <!-- RSS -->
-    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<!-- SCRIPTS -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
 
-    <!-- SCRIPTS -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
-
-    <!-- RESPOND -->
-    <!--[if lt IE 9]>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
-    <![endif]-->
+<!-- RESPOND -->
+<!--[if lt IE 9]>
+<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+<![endif]-->
 
     
 </head>
