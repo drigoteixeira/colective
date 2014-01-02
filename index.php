7b4ff5aaa4893ede93acb012a1a8d566deafb80c
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2014-01-02 15:13:50 -0200

    Atualização do script para abertura dos links em uma nova aba

--- index.php
+++ index.php
@@ -2,29 +2,29 @@
 
 	<?php if (have_posts()) : ?>
 	<?php while (have_posts()) : the_post(); ?>
-<div class="container-12">
-			<div class="grid-12">
-				<section class="posts">
-					<article class="post">
-						<div class="post-meta">
-							<span class="post-date"><?php the_time('j M, Y') ?></span>
-						</div>
-						<h2 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-						<div class="post-content">
-							<?php the_excerpt(); ?>
-						</div>
-					</article>
-				</section>
-			</div>
-</div>
-			<?php endwhile; ?>
-
-			<div class="grid-12">
-				<div class="pagination">
-					<?php pagination('«', '»'); ?>
+	<div class="container-12">
+				<div class="grid-12">
+					<section class="posts">
+						<article class="post">
+							<div class="post-meta">
+								<span class="post-date"><?php the_time('j M, Y') ?></span>
+							</div>
+							<h2 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+							<div class="post-content">
+								<?php the_excerpt(); ?>
+							</div>
+						</article>
+					</section>
 				</div>
-			</div>
-			
-			<?php endif; ?>
+	</div>
+	<?php endwhile; ?>
+
+	<div class="grid-12">
+		<div class="pagination">
+			<?php pagination('«', '»'); ?>
+		</div>
+	</div>
+	
+	<?php endif; ?>
 
 <?php get_footer(); ?>
