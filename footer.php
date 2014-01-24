8ec79c6bb882d665c7cba5e763ef46508a5f3fef
Rodrigo Teixeira <rodrigo@a2comunicacao.com.br>
2014-01-24 10:36:34 -0200

    Atualização do código do Analytics

--- footer.php
+++ footer.php
@@ -34,18 +34,21 @@
         <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
         <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-        <script type="text/javascript">
+        
 
-          var _gaq = _gaq || [];
-          _gaq.push(['_setAccount', 'UA-32844537-1']);
-          _gaq.push(['_trackPageview']);
+        <!-- ANALYTICS UNIVERSAL -->
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
+          ga('create', 'UA-32844537-1', 'drigoteixeira.com');
+          ga('send', 'pageview');
 
         </script>
+    
+
+
     </body>
 </html>
